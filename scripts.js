e9cde927ebb7df0cef8e31b26d2a9b0ef25cfea2
nathaniel <njm2015@gmail.com>
2018-06-24 14:31:51 -0500

    temp push to github

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function myMap() {
+	var mapOptions = {
+		center: new google.maps.LatLng(40.1164, 88.2434),
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+var map = new google.maps.Map(document.getElementById("map-container"))
+}
